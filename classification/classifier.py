c38cb1a2fb1fa7abd5706f165decfbcf333121cc
Steven Ortiz <steven_developer@hotmail.com>
2014-10-22 21:38:10 +0200

    Add multiprocessing to classifier.py

--- classification/classifier.py
+++ classification/classifier.py
@@ -7,6 +7,9 @@ import io
 import os
 from nltk.corpus import brown, stopwords
 from collections import Counter
+from itertools import chain
+from multiprocessing import Pool
+from itertools import zip_longest
 import json
 
 import cProfile as profile
@@ -20,6 +23,18 @@ datavars = dict()
 corpus = brown
 CATEGORIES = corpus.categories()
 FILENAME = 'classifier.json'
+CPU_COUNT = os.cpu_count() if os.cpu_count() is not None else 1
+FID_CATEGORY = dict()
+for category in corpus.categories():
+    FIDS = corpus.fileids(categories=category)
+    FID_CATEGORY.update(dict(zip_longest(FIDS, [], fillvalue=category)))
+
+def split_list(initial_list, parts):
+    # assumes: there are more elements than parts
+    split = list()
+    for p in range(parts):
+        split.append(tuple(initial_list[p::parts]))
+    return split
 
 def get_all_words():
     if 'all_words' not in datavars and os.path.exists(FILENAME):
@@ -27,7 +42,7 @@ def get_all_words():
             datavars.update(json.load(datafile))
     if 'all_words' not in datavars: # even after loading file
         all_words = sorted(list(
-            set(w.lower() for w in corpus.words() if w.isalnum()) -
+            set(w.lower() for w in corpus.words() if w.isalnum() and len(w) > 3) -
             set(stopwords.words())
             ))
         datavars['all_words'] = all_words
@@ -35,18 +50,29 @@ def get_all_words():
             json.dump(datavars, datafile, indent=4)
     return datavars['all_words']
 
-def get_samples_category(all_words):
+
+def get_samples_from_fileids(fid_list):
+    samp = dict()
+    for fid in fid_list:
+        count = Counter(corpus.words(fid))
+        samp.update({FID_CATEGORY[fid]: tuple(count[word] for word in ALL_WORDS)})
+    return samp
+
+def get_samples_category():
     if 'samples_category' not in datavars and os.path.exists(FILENAME):
         with open(FILENAME, 'r') as datafile:
             datavars.update(json.load(datafile))
     if 'samples_category' not in datavars: # even after loading file
-        samples_category = dict()
-        for category in corpus.categories():
-            samples = list()
-            for fid in corpus.fileids(categories=category):
-                count = Counter(corpus.words(fid))
-                samples.append(tuple(count[word] for word in all_words))
-            samples_category[category] = tuple(samples)
+        cpu_jobs = split_list(corpus.fileids(), CPU_COUNT)
+        cpu_pool = Pool(CPU_COUNT)
+        scd = cpu_pool.map(get_samples_from_fileids, cpu_jobs)
+        #samples_category = dict(zip_longest(corpus.categories(), [], fillvalue=list()))
+        #for sample in sample_list:
+            #samples_category[fid_category[sample[0]]].append(tuple(sample[1]))
+            #samples_category[category] +=
+            #{fid_category[sample[0]]: sample[1]
+                #for sample in chain.from_iterable(sample_list)}
+        samples_category = {cat:[d.get(cat) for d in scd] for cat in {cat for d in scd for cat in d}}
         datavars['samples_category'] = samples_category
         with open(FILENAME, 'w') as datafile:
             json.dump(datavars, datafile, indent=4)
@@ -62,7 +88,8 @@ def get_SVM_samples(samples_categories):
             svm_predictions.append(cat)
     return tuple(svm_samples), tuple(svm_predictions)
 
-SAMPLES_CATEGORY = get_samples_category(get_all_words())
+ALL_WORDS = get_all_words()
+SAMPLES_CATEGORY = get_samples_category()
 
 SAMPLES, PREDICTIONS = get_SVM_samples(SAMPLES_CATEGORY)
 
