2274901fd4a8bc76084a034b9c4e7e0d11dbc9dd
Steven Ortiz <steven_developer@hotmail.com>
2014-10-24 00:41:53 +0200

    Add SVM to classifier.py. Remove multiprocessing
    * Add SVM with RBF kernel
      - Poor performance (0.15151515 accuracy) probably due to small dataset
        (Brown corpus: 500 texts)
    
    * Remove multiprocessing

--- classification/classifier.py
+++ classification/classifier.py
@@ -1,13 +1,13 @@
 import nltk
 #import numpy
 #import matplotlib
-#import sklearn
+from sklearn.svm import SVC
 import pstats
 import io
 import os
+import random
 from nltk.corpus import brown, stopwords
 from collections import Counter
-from multiprocessing import Pool # pylint: disable=no-name-in-module
 from itertools import zip_longest
 import json
 
@@ -23,17 +23,6 @@ corpus = brown
 CATEGORIES = corpus.categories()
 FILENAME = 'cached_data.json'
 CPU_COUNT = os.cpu_count() if os.cpu_count() is not None else 1
-FID_CATEGORY = dict()
-for category in corpus.categories():
-    FIDS = corpus.fileids(categories=category)
-    FID_CATEGORY.update(dict(zip_longest(FIDS, [], fillvalue=category)))
-
-def split_list(initial_list, parts):
-    # assumes: there are more elements than parts
-    split = list()
-    for p in range(parts):
-        split.append(tuple(initial_list[p::parts]))
-    return split
 
 def get_all_words():
     if 'all_words' not in datavars and os.path.exists(FILENAME):
@@ -50,41 +39,60 @@ def get_all_words():
     return datavars['all_words']
 
 
-def get_samples_from_fileids(fid_list):
-    samp = dict()
-    for fid in fid_list:
-        count = Counter(corpus.words(fid))
-        samp.update({FID_CATEGORY[fid]: tuple(count[word] for word in ALL_WORDS)})
-    return samp
-
 def get_samples_category():
     if 'samples_category' not in datavars and os.path.exists(FILENAME):
         with open(FILENAME, 'r') as datafile:
             datavars.update(json.load(datafile))
     if 'samples_category' not in datavars: # even after loading file
-        cpu_jobs = split_list(corpus.fileids(), CPU_COUNT)
-        cpu_pool = Pool(CPU_COUNT)
-        scd = cpu_pool.map(get_samples_from_fileids, cpu_jobs)
-        samples_category = {cat:[d.get(cat) for d in scd] for cat in {cat for d in scd for cat in d}}
+        samples_category = dict()
+        for category in corpus.categories():
+            samples = list()
+            for fid in corpus.fileids(categories=category):
+                count = Counter(corpus.words(fid))
+                samples.append(tuple(count[word] for word in datavars['all_words']))
+            samples_category[category] = tuple(samples)
         datavars['samples_category'] = samples_category
         with open(FILENAME, 'w') as datafile:
             json.dump(datavars, datafile, indent=4)
     return datavars['samples_category']
 
-def get_SVM_samples(samples_categories):
-    svm_samples = list()
-    svm_predictions = list()
-    for category, samples in samples_categories.items():
-        cat = CATEGORIES.index(category)
-        for samp in samples:
-            svm_samples.append(samp)
-            svm_predictions.append(cat)
-    return tuple(svm_samples), tuple(svm_predictions)
+def get_SVM_samples_predictions(samples_categories):
+    if 'svm_samples_predictions' not in datavars and os.path.exists(FILENAME):
+        with open(FILENAME, 'r') as datafile:
+            datavars.update(json.load(datafile))
+    if 'svm_samples_predictions' not in datavars: # even after loading file
+        svm_samples = list()
+        svm_predictions = list()
+        for category, samples in samples_categories.items():
+            cat = CATEGORIES.index(category)
+            for samp in samples:
+                svm_samples.append(samp)
+                svm_predictions.append(cat)
+        indexes = list(range(len(svm_samples)))
+        random.shuffle(indexes)
+        svm_samples_shuf = list()
+        svm_predictions_shuf = list()
+        len80pc = int(len(indexes) * 0.8)
+        for i in indexes:
+            svm_samples_shuf.append(svm_samples[i])
+            svm_predictions_shuf.append(svm_predictions[i])
+        # 80% of samples will be used for training, and the remaining 20% for testing
+        svm_samples_predictions = (tuple(svm_samples_shuf[:len80pc]), tuple(svm_predictions_shuf[:len80pc]),
+                tuple(svm_samples_shuf[len80pc+1:]), tuple(svm_predictions_shuf[len80pc+1:]))
+        datavars['svm_samples_predictions'] = svm_samples_predictions
+        with open(FILENAME, 'w') as datafile:
+            json.dump(datavars, datafile, indent=4)
+    return datavars['svm_samples_predictions']
 
 ALL_WORDS = get_all_words()
 SAMPLES_CATEGORY = get_samples_category()
 
-SAMPLES, PREDICTIONS = get_SVM_samples(SAMPLES_CATEGORY)
+TRAIN_SAMP, TRAIN_PRED, TEST_SAMP, TEST_PRED = get_SVM_samples_predictions(SAMPLES_CATEGORY)
+print('Training...')
+CLASSIFIER = SVC(kernel='rbf')
+CLASSIFIER.fit(TRAIN_SAMP, TRAIN_PRED)
+print('Calculating mean accuracy')
+print(CLASSIFIER.score(TEST_SAMP, TEST_PRED))
 
 ################################################################################
 
