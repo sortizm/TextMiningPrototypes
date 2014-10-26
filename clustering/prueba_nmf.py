66ffd920c036cfe9c04526a41fa59f066c6576b9
xinofekuator <xinofekuator@gmail.com>
2014-10-26 19:30:44 +0100

    Added notes.txt and modified prueba_nmf.py
    *Now the matrix of frecuencies is transformed using the TF*IDF heuristic
    *The jSon now includes the stemming
    *Added notes about the time spent running with the brown corpus

--- clustering/prueba_nmf.py
+++ clustering/prueba_nmf.py
@@ -8,6 +8,7 @@ import numpy
 import matplotlib
 import sklearn
 import json
+#from scipy import sparse
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.corpus import brown
@@ -22,48 +23,43 @@ profiler = profile.Profile()
 profiler.enable()
 
 """
-Importamos el corpus con el que vamos a trabajar
+
 """
 #C:\Users\Ignacio\Desktop\corpusTweets
 #C:\Users\Ignacio\Downloads\corpusPrueba
+#C:\Users\Ignacio\Downloads\corpusCine\corpusTxt
 
 
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
-corpusdir = r"C:\Users\Ignacio\Downloads\corpusPrueba" #CHANGE THIS (local adress of the corpus)
+corpusdir = r"C:\Users\Ignacio\Downloads\corpusCine\corpusTxt" #CHANGE THIS (local adress of the corpus)
 newcorpus = PlaintextCorpusReader(corpusdir, ".*") 
+
+###############
+
+#Global constants and variables
 
 n_topics=7
 n_top_words=10
 
-corpus=newcorpus #selecting the corpus to use
+corpus=brown #selecting the corpus to use
 datavars = dict()
 FILENAME = 'cached_data.json'
-t0 = time() #initializing the timer
 
 files=corpus.fileids()
 lenFiles=len(files)
-"""
-We are going to create a filter to reduce the number of words we are going to be looking at
-filtro2: Keeps the words which appear in the range of a percentage (for example: if a word
-        appears in less than 5% of the texts or in more than 95% of the text, it is discarded
-        because is not relevant enough) 
 
-We can put a ceiling in the frecuency of a word in a text (divide its frecuency to the total number of words maybe) FEATURE REDUCTION??
-Elias takes only the words with a certain percentage of appearance in the text (good idea)
-Correct the minimum percentage(maybe it s not correct to include it) and reduce the maximun percentage
-TRY OTHER ALGORTITHMS
-
-porcentajes que dependan del numero de textos y del numero de categorias (PROBAR: textos*0.5*categorias/250)
-"""
-
-#it returns a list with the words filtered and another list with all the words and its repetition in each text
 
 def preprocess():
+    """
+    Filters the words in the corpus. We discard numbers, very short words and words appearing in less than 5% of the texs 
+    and in more than the 95% of the texts. (NEED TO BE CHANGED ---> the percentage needs to change depending on the number of texts)
+    return: a 2-dimension list with the filtered words and the filtered texts
+    """
     if ('filtered_words' not in datavars or 'wordsTexts' not in datavars) and os.path.exists(FILENAME):
         with open(FILENAME, 'r') as datafile:
             datavars.update(json.load(datafile))
     if 'filtered_words' not in datavars or 'wordsTexts' not in datavars: # even after loading file
-        print('No json found')
+        print('No json found in the preprocess')
         wordsTexts=[] #list of counts
         filteredWords=[]
         tuplaAux=()
@@ -89,30 +85,50 @@ def preprocess():
     return [datavars['filtered_words'],datavars['wordsTexts']]
     
 
-
 def stemming(palabras,wordsTexts):
-    stemmer1 = SnowballStemmer("spanish")
-    stemWordText=[[] for i in range(len(wordsTexts))]
-    stemPalabras=[]
-    for i in palabras:
-        try:
-            stemPalabras.append(stemmer1.stem(i))
-        except IndexError:
-            pass 
-    i=-1
-    for text in wordsTexts:
-        i=i+1
-        for word in text:
+    """
+    Reduces the amount of filtering words keeping only their stems
+    return: a list with the words and the texts filtered
+    CAN BE IMPROVED USING A SPARSE MATRIX
+    """
+    if ('stemmed_words' not in datavars or 'stemmed_texts' not in datavars) and os.path.exists(FILENAME):
+        with open(FILENAME, 'r') as datafile:
+            datavars.update(json.load(datafile))
+    if 'stemmed_words' not in datavars or 'stemmed_texts' not in datavars: # even after loading file
+        print('No json found in the stemming')
+        stemmer1 = SnowballStemmer("spanish")
+        stemWordText=[[] for i in range(len(wordsTexts))]
+        stemPalabras=[]
+        for i in palabras:
             try:
-                stemWordText[i].append(stemmer1.stem(word))
+                stemPalabras.append(stemmer1.stem(i))
             except IndexError:
-                pass
+                pass 
+        i=-1
+        for text in wordsTexts:
+            i=i+1
+            for word in text:
+                try:
+                    stemWordText[i].append(stemmer1.stem(word))
+                except IndexError:
+                    pass
+                
+        palabras=list(set(stemPalabras))
+        wordsTexts=stemWordText
+        datavars['stemmed_words'] = palabras
+        datavars['stemmed_texts'] = wordsTexts
+        with open(FILENAME, 'w') as datafile:
+            json.dump(datavars, datafile, indent=4)
             
-    palabras=list(set(stemPalabras))
-    wordsTexts=stemWordText
-    return [palabras,wordsTexts]
+
+    return [datavars['stemmed_words'],datavars['stemmed_texts']]
+
 
 def create_matrix(palabras,wordsTexts):
+    """
+    Creates a matrix with the count of each word. The matrix rows are the words and the matrix columns are the corpus files.
+    return: the matrix and two lists with the labels of the rows and columns
+    """
     filas=lenFiles #numero de filas de la matriz
     columnas=len(palabras)
     idFilas=[]
@@ -135,29 +151,82 @@ def create_matrix(palabras,wordsTexts):
     return [matriz,idFilas,idColumnas]
 
 
-def factorization(matriz):
+def idf(matriz):
+    """
+    Transform the word frequencies in the matrix to reflect to add information using the TF*IDF heuristic
+    return: the modified matrix
+    """
+
+    #could be improved using sparse matrix
+    #matriz=sparse.csr_matrix(matriz)
+    filas=matriz.shape[0] #number of files
+    columnas=matriz.shape[1] #number of words
+    lista_aux=[None]*columnas
+    for j in range(columnas):
+        count=0
+        for i in range(filas):
+           if matriz[i][j]!=0:
+               count=count+1
+        lista_aux[j]=count    
+    
+    idf_matriz= numpy.empty([filas,columnas],dtype=numpy.float64)
+    for i in range(filas):
+        for j in range(columnas):
+            idf_matriz[i][j]= numpy.log(matriz[i][j]+1) * numpy.log(filas/lista_aux[j])
+                
+    return idf_matriz
+    
+  
+def factorization_NMF(matriz):
+    """
+    Applies the non-negative matrix factorization
+    return: The characteristics matrix and the weight matrix
+    """      
     nmf = NMF(n_components=n_topics, random_state=1).fit(matriz)
     F=nmf.components_ #matriz de caracteristicas (filas:caracteristicas , columnas:palabras)
     W=nmf.fit_transform(matriz) #matriz de pesos (filas:textos , columnas:caracteristicas)
     return [F,W]
-    
+
+  
 def print_topics(F,W,idFilas,idColumnas):
+    """
+    Prints the topics and the text topics in two files
+    """  
     with open("Topics.txt", "w") as text_file:
         for topic_idx, topic in enumerate(F):
             print("Topic {}: ".format(topic_idx), file=text_file)
             print(" ".join([idColumnas[i] for i in topic.argsort()[:-n_top_words - 1:-1]]), file=text_file) 
+        text_file.close()
     with open("TextTopics.txt", "w") as text_file:
         for text, category in enumerate(W):
             print("Text #{}:".format(idFilas[text]),end=' ',file=text_file)
             print("Category #{}".format(category.argsort()[len(category)-1]),file=text_file) #tomo el mayor(esta el ultimo al ordenarlo)
+        text_file.close()
 
+#############
+#Delete the jSon file is the corpus is changed or modified
+t0 = time()
+print("Preprocessing")
 a=preprocess()
+print("done in %0.3fs." % (time() - t0))
+print("Stemming")
 s=stemming(a[0],a[1])
+print("done in %0.3fs." % (time() - t0))
+print("Creating data matrix")
 m=create_matrix(s[0],s[1])
-res=factorization(m[0])
+print("done in %0.3fs." % (time() - t0))
+print("Converting matrix values to TF*IDF")
+f=idf(m[0])
+print("done in %0.3fs." % (time() - t0))
+print("Non-negative factorization")
+res=factorization_NMF(f)
+print("done in %0.3fs." % (time() - t0))
+print("Printing results")
 print_topics(res[0],res[1],m[1],m[2])
+print("done in %0.3fs." % (time() - t0))
 
 profiler.disable()
 stream= open('stats.txt','w') 
 ps = pstats.Stats(profiler, stream=stream).sort_stats('cumulative')
 ps.print_stats()
+stream.close()
